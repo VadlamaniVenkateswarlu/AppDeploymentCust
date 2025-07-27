17f2dd7e0b6fdae957016bf12f4645dd7a2873d0
Venkateswarlu <venkateshwarluvadlamani233@gmail.com>
2025-07-27 17:32:07 +0530

    changed from build to dist

--- customerUI/Backend/server.js
+++ customerUI/Backend/server.js
@@ -15,10 +15,10 @@ app.use(cors());
 app.use(express.json());
 //app.use(express.static(path.join(__dirname, "client", "build")));
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "../build"))); // ← CORRECTED
+app.use(express.static(path.join(__dirname, "../dist"))); // ← CORRECTED
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build", "index.html")); // ← CORRECTED
+  res.sendFile(path.join(__dirname, "../dist", "index.html")); // ← CORRECTED
 });
 
 
